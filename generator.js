df7640a2e6577cf4cb7be06311476f255e2bf0b1
Matthew Wright <matthewbwright@gmail.com>
2019-01-25 14:23:45 -0700

    sets up generator.js

---
+++ generator.js
@@ -0,0 +1,57 @@
+//Code that generates the random lorem ipsum text
+
+//Create a new object called loremIpsum by invoking the GenerateNewText constructor function
+const loremIpsum = new GenerateNewText();
+
+//Constructor function that creates an object with the sentences property
+function GenerateNewText() {
+  //Add property tot he object
+  this.sentences = [
+    'Awesome quote #1.',
+    'Second awesome quote.',
+    'Yet another great quote.',
+    'Ok, last quote.'
+  ];
+}
+
+//Method to the GenerateNewTExt constructor function that generates a random sentence
+GenerateNewText.prototype.getRandomSentence = function() {
+  let randomSentence = this.sentences[
+    Math.floor(Math.random() * this.sentences.length)
+  ];
+  return randomSentence;
+};
+
+//Method to the GenerateNewText constructor function that generates a paragraph from random sentences
+GenerateNewText.prototype.getParagraph = function() {
+  let paragraph = '';
+  //Set the minimum number of words
+  let minimumCharacterLength = 250;
+  let firstSentence = true;
+  while (paragraph.length < minimumCharacterLength) {
+    if (firstSentence) {
+      paragraph = paragraph.concat(this.getRandomSentence());
+      firstSentence = false;
+    } else {
+      paragraph = paragraph.concat(' ' + this.getRandomSentence());
+    }
+  }
+  return paragraph;
+};
+
+//Method to the GenerateNewText constructor function that generates multiple paragraphs from paragraphs
+GenerateNewText.prototype.getAllParagraphs = function(numberOfParagraphs) {
+  let allParagraphs = [];
+  //Generate the number of paragraphs as specified by the user
+  while (allParagraphs.length < numberOfParagraphs) {
+    allParagraphs.push(this.getParagraph());
+  }
+  //Convert array into HTML string
+  let paragraphHTML = '';
+  allParagraphs.forEach(function(paragraph) {
+    paragraphHTML += '<p>' + paragraph + '</p>';
+  });
+  return paragraphHTML;
+};
+
+module.exports = loremIpsum;
